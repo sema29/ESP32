7ef454a2d98d24162f14d1f5d9289719c797f347
sema29 <semanurboz5@gmail.com>
2022-07-22 14:37:56 +0300

    update

--- pwm/introduction.py
+++ pwm/introduction.py
@@ -16,3 +16,14 @@ time.sleep(1)
 pwm0.duty(1023)
 time.sleep(1)
 
+
+
+---------------------------
+
+led = PWM(Pin(14), 5000)
+
+while True:
+ for dutyCycle in range(0, 1024):
+  led.duty(dutyCycle)
+  print(dutyCycle)
+  sleep(0.2)
